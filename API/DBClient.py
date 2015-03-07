e261a43305f5a9ca173214eb804c1d4fa12a29d5
xuhe <xuhe@yahoo-inc.com>
2015-03-06 22:33:31 -0800

    Check in API code

---
+++ API/DBClient.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient
+import json
+# client = MongoClient()
+# db = client.test_database
+# collection = db.test_collection
+# print collection.find_one()
+
+
+class DBClient:
+	client=None
+	databaseName="TrendingYelp"
+	collectionName="TrendingYelp"
+	collection=None
+	def __init__(self):
+		client = MongoClient('localhost', 27017)
+		db = client[self.databaseName]
+		self.collection = db[self.collectionName]
+
+	def get(self, name):
+		# print self.collection.find_one({"name": "%s" % name})
+		print "DBClient "+name
+		return self.collection.find_one({"name": "%s" % name})
+
+	def put(self, content):
+		jsonData=json.loads(content)
+		dataID=self.collection.insert(jsonData)
+		return dataID
+
+
+if __name__ == '__main__':
+
+	client=DBClient()
+	#test for put
+	# testJson=open("testJson.json", 'r')
+	# content=testJson.readlines()[0]
+	# # print content
+	# print client.put(content)
+
+	#test for get
+	print client.get("test")
